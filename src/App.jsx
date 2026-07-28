b9e2f22de70d11bf6bea1e0c6056415c7db23274
unknown <met.esso@gmail.com>
2026-07-28 18:07:22 +0000

    bump version V2.1

--- src/App.jsx
+++ src/App.jsx
@@ -1171,7 +1171,7 @@ function LoginScreen({ clients, users, complaints, onAdminLogin, onTechLogin, on
         <h1 style={{ textAlign: "center", marginBottom: 4, fontSize: 22, fontWeight: 700, color: "#FFE9A8", letterSpacing: ".2px" }}>APESPOT WI-FI</h1>
         <div className="sub" style={{ textAlign: "center", marginBottom: 6 }}>Choisis ton espace</div>
         <div style={{ textAlign: "center", marginBottom: 26 }}>
-          <span className="app-version-badge">V2.0</span>
+          <span className="app-version-badge">V2.1</span>
         </div>
 
         {!selected && (
